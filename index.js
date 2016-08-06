d5f8431b811770db523a415cfef13397688e63a0
Stephanie Chow <stephaniechow36@gmail.com>
2016-08-06 18:33:40 -0400

    Initial commit with test endpoint

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.post('/', function(req, res) {
+    res.send("Hi from mtgslackbot");
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("Listening on port " + port + "...");
+});
